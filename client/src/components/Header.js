ad5e300b4fd2ea98cdbbedef6e315e478f4585c7
Jere <jere@flamero.fi>
2017-04-04 03:03:46 +0300

    Navbar header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from '../../public/logo.png';
 import { Link } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <ul className="nav justify-content-center">
-        <h2>HQ Calendar</h2>
-          <li className="nav-item">
-            <Link to="/events" className="nav-link">Events</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/events/new" className="nav-link">New event</Link>
-          </li>
-          <li className="nav-item">
-            <span className="nav-link">Users</span>
-          </li>
-        </ul>
+      <div>
+      <Navbar color="inverse" className="mb-5" inverse toggleable>
+        <NavbarToggler right onClick={this.toggle} />
+        <NavbarBrand tag={Link} to="/">
+          <img src={logo} className="logo" height="50px" alt="HQ" />
+        </NavbarBrand>
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="mx-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/events">Events</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/events/new">New event</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
       </div>
     );
   }
