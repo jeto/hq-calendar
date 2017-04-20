b467a1e47ac995f3ee48e0d5ef87f633ebd68583
Jere <jere@flamero.fi>
2017-04-20 20:23:24 +0300

    Comment deletion fixes

--- client/src/reducers/reducer_comments.js
+++ client/src/reducers/reducer_comments.js
@@ -1,11 +1,9 @@
-import { FETCH_COMMENTS, CREATE_COMMENT } from '../actions/types';
+import { FETCH_COMMENTS } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_COMMENTS:
       return [ ...action.payload.data ]
-    case CREATE_COMMENT:
-      return [ action.payload.data, ...state ]
     default:
       return state;
     }
